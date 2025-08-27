41242db4afdc012599e11403a64a18ec574fee7b
IFAZANE <85297936+IFAZANE@users.noreply.github.com>
2025-08-27 21:26:40 +0000

    models.py

--- fafa/models.py
+++ fafa/models.py
@@ -4,12 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Subscription(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # ✅ Auto-incrément
     uuid = db.Column(db.String(36), default=lambda: str(uuid.uuid4()), unique=True)
     nom = db.Column(db.String(50), nullable=False)
     prenom = db.Column(db.String(50), nullable=False)
     telephone = db.Column(db.String(20), nullable=False)
     ville = db.Column(db.String(50), nullable=False)
     produit = db.Column(db.String(20), nullable=False)
-
-
