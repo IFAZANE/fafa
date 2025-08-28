8b9b1ab0ba5f1950d52a3030dfcf7f219bfeaf83
IFAZANE <85297936+IFAZANE@users.noreply.github.com>
2025-08-28 15:29:48 +0000

    models.py

--- fafa/models.py
+++ fafa/models.py
@@ -1,16 +1,41 @@
-import uuid
+#import uuid
+#from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy import Enum
+
+#db = SQLAlchemy()
+
+#class Subscription(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)  # auto-incrément
+#    uuid = db.Column(db.String(36), unique=True, nullable=False)
+#    nom = db.Column(db.String(50), nullable=False)
+#    prenom = db.Column(db.String(50), nullable=False)
+#    telephone = db.Column(db.String(20), nullable=False)
+#    ville = db.Column(db.String(50), nullable=False)
+#    produit = db.Column(Enum('Gold', 'Silver', 'Bronze', name='produit_enum'), nullable=False)
+
+
+
+import random
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Enum
 
 db = SQLAlchemy()
 
+def generate_unique_code():
+    """Génère un code unique à 6 chiffres."""
+    while True:
+        code = str(random.randint(100000, 999999))
+        if not Subscription.query.filter_by(code=code).first():
+            return code
+
 class Subscription(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # auto-incrément
-    uuid = db.Column(db.String(36), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(6), unique=True, default=generate_unique_code)
     nom = db.Column(db.String(50), nullable=False)
     prenom = db.Column(db.String(50), nullable=False)
     telephone = db.Column(db.String(20), nullable=False)
     ville = db.Column(db.String(50), nullable=False)
-    produit = db.Column(Enum('Gold', 'Silver', 'Bronze', name='produit_enum'), nullable=False)
+    produit = db.Column(db.String(20), nullable=False)
+
+
 
 
