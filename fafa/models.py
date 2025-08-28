4c1df1a875fa87249f5cc7e94d73b5689e2cefe2
IFAZANE <85297936+IFAZANE@users.noreply.github.com>
2025-08-28 17:15:32 +0000

    models.py

--- fafa/models.py
+++ fafa/models.py
@@ -1,40 +1,19 @@
-#import uuid
-#from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy import Enum
-
-#db = SQLAlchemy()
-
-#class Subscription(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)  # auto-incrément
-#    uuid = db.Column(db.String(36), unique=True, nullable=False)
-#    nom = db.Column(db.String(50), nullable=False)
-#    prenom = db.Column(db.String(50), nullable=False)
-#    telephone = db.Column(db.String(20), nullable=False)
-#    ville = db.Column(db.String(50), nullable=False)
-#    produit = db.Column(Enum('Gold', 'Silver', 'Bronze', name='produit_enum'), nullable=False)
-
-
-
-import random
+import uuid
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 
 db = SQLAlchemy()
 
-def generate_unique_code():
-    """Génère un code unique à 6 chiffres."""
-    while True:
-        code = str(random.randint(100000, 999999))
-        if not Subscription.query.filter_by(code=code).first():
-            return code
-
 class Subscription(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # auto-incrément
+    uuid = db.Column(db.String(36), unique=True, nullable=False)
     nom = db.Column(db.String(50), nullable=False)
     prenom = db.Column(db.String(50), nullable=False)
     telephone = db.Column(db.String(20), nullable=False)
     ville = db.Column(db.String(50), nullable=False)
-    produit = db.Column(db.String(20), nullable=False)
-    code = db.Column(db.String(6), unique=True, default=generate_unique_code)
+    produit = db.Column(Enum('Gold', 'Silver', 'Bronze', name='produit_enum'), nullable=False)
+
+
 
 
 
