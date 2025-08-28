b6c2f0c8349f60405818c2c93e7f9302e1097387
IFAZANE <85297936+IFAZANE@users.noreply.github.com>
2025-08-28 20:25:46 +0000

    forms.py

--- fafa/forms.py
+++ fafa/forms.py
@@ -1,18 +1,18 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Regexp
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
 
 class SouscriptionForm(FlaskForm):
-    nom = StringField("Nom", validators=[DataRequired()])
-    prenom = StringField("Prénom", validators=[DataRequired()])
-    telephone = StringField("Téléphone", validators=[
-        DataRequired(),
-        Regexp(r'^\d{8,15}$', message="Numéro de téléphone invalide")
-    ])
-    ville = StringField("Ville", validators=[DataRequired()])
-    produit = SelectField("Produit", choices=[
-        ("Option1", "15 000F/ans"),
-        ("Option2", "20 000F/ans")
-    ], validators=[DataRequired()])
-    recaptcha = RecaptchaField()
-    submit = SubmitField("Souscrire")
+    nom = StringField('Nom', validators=[DataRequired()])
+    prenom = StringField('Prénom', validators=[DataRequired()])
+    telephone = StringField('Téléphone', validators=[DataRequired()])
+    ville = StringField('Ville', validators=[DataRequired()])
+    
+    # On ne propose que Option1 et Option2
+    produit = SelectField(
+        'Produit',
+        choices=[('Option1', 'Option1 (15 000 F/ans)'), ('Option2', 'Option2 (20 000 F/ans)')],
+        validators=[DataRequired()]
+    )
+    
+    submit = SubmitField('Souscrire')
