c1aef994d758d7bf9805066dbeca6569e61e9e5e
IFAZANE <85297936+IFAZANE@users.noreply.github.com>
2025-08-28 18:50:19 +0000

    forms.py

--- fafa/forms.py
+++ fafa/forms.py
@@ -1,34 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Regexp
 
 class SubscriptionForm(FlaskForm):
-    nom = StringField("Nom", validators=[
-        DataRequired(message="Le nom est requis.")
-    ])
-    
-    prenom = StringField("Prénom", validators=[
-        DataRequired(message="Le prénom est requis.")
-    ])
-    
+    nom = StringField('Nom', validators=[DataRequired()])
+    prenom = StringField('Prénom', validators=[DataRequired()])
+    telephone = StringField('Téléphone', validators=[DataRequired()])
+    ville = StringField('Ville', validators=[DataRequired()])
+    produit = SelectField('Produit', choices=[('Bronze', 'Bronze'), ('Silver', 'Silver'), ('Gold', 'Gold')])
+    submit = SubmitField('Souscrire')
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class SouscriptionForm(FlaskForm):
+    nom = StringField("Nom", validators=[DataRequired()])
+    prenom = StringField("Prénom", validators=[DataRequired()])
+    recaptcha = RecaptchaField()  # <-- captcha
     telephone = StringField("Téléphone", validators=[
-        DataRequired(message="Le numéro de téléphone est requis."),
-        Regexp(r'^\d{8,15}$', message="Numéro de téléphone invalide (8 à 15 chiffres attendus).")
+        DataRequired(),
+        Regexp(r'^\d{8,15}$', message="Numéro de téléphone invalide")
     ])
-    
-    ville = StringField("Ville", validators=[
-        DataRequired(message="La ville est requise.")
-    ])
+    ville = StringField("Ville", validators=[DataRequired()])
     
     produit = SelectField("Produit", choices=[
-        ("", "Sélectionnez un produit"),
-        ("Option1", "15 000F/ans"),
-        ("Option2", "20 000F/ans")
-        
-    ], validators=[
-        DataRequired(message="Veuillez sélectionner un produit.")
-    ])
-    
+        ("Bronze", "Bronze"),
+        ("Silver", "Silver"),
+        ("Gold", "Gold")
+    ], validators=[DataRequired()])
+
     recaptcha = RecaptchaField()
     submit = SubmitField("Souscrire")
+
 
